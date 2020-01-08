eac9ae833e7fc1775ba58d152b984470dc7bc8a1
Bruna Grassi <bruna.grassi@soulasalle.com.br>
2020-01-07 23:20:55 -0300

    dollar value + tax working; results of dollar and real working

--- client/src/app/modules/currency/currency/currency.component.ts
+++ client/src/app/modules/currency/currency/currency.component.ts
@@ -10,7 +10,11 @@ import { Dollar } from './models/dollar';
   styleUrls: ['./currency.component.scss']
 })
 export class CurrencyComponent implements OnInit {
-  public currency: ICurrency = {USD: new Dollar()};
+  public currency: ICurrency = { USD: new Dollar() };
+  public price: number = 0;
+  public tax: number = 0;
+  public americanPrice: number;
+  public brazilianPrice: number;
 
   constructor(public dollarService: CurrencyService) { }
 
@@ -18,9 +22,32 @@ export class CurrencyComponent implements OnInit {
     this.getDollarPrice();
   }
 
-  private getDollarPrice() {
+  private getDollarPrice(): void {
     this.dollarService.getDollarPrice().subscribe(result => {
       this.currency = result;
+      console.log(result.USD.create_date);
     });
+  }
+
+  public priceInput(value: number): void {
+	  this.price = value;
+  }
+
+  public taxInput(value: number): void {
+	  this.tax = value;
+  }
+
+  private getAmericanPrice() {
+    if(this.price && this.tax != 0)
+      return this.americanPrice = this.price * (this.tax/100 + 1);
+    
+    return this.americanPrice = 0;
+  }
+  
+  private getBrazilianPrice() {
+    if(this.price && this.tax != 0)
+      return this.brazilianPrice = (this.price * this.currency.USD.high) * (this.tax/100 + 1);
+    
+    return this.brazilianPrice = 0;
   }
 }
